dd4c1823ff7b954783c92c0be69784e9e16585b7
mrj666 <33885009+mrj666@users.noreply.github.com>
2021-08-16 21:38:01 +0800

    Update case_pick.py
    add draft

--- case_pick.py
+++ case_pick.py
@@ -1,1 +1,28 @@
 
+import re
+import xlrd
+
+case_data = 'auto_test_list'
+
+table = xlrd.open_workbook(case_data) #read excel value
+sutable = table.sheet_by_name('SU')
+
+mcs_list = [0, 1, 9]
+dcm_list = [0, 1]
+he_list = [0, 1, 2]
+gi_list = [0, 1, 3]
+
+mcs_col = 0  # line A means 0
+dcm_col = 1
+he_col = 2
+gi_col = 3
+
+table_lines = 200
+for mcs in mcs_list:
+    for dcm in dcm_list:
+        for he in he_list:
+            for gi in gi_list:
+                for line in range(0, table_lines):
+                    tmp_mcs = sutable.cell_value(table_lines, mcs_col)
+                    if str(mcs) == tmp_mcs:
+                        tmp_dcm = sutable.cell_value(table_lines, dcm_col)
